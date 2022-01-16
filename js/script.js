dbf0f924245a43d0dde68419ddfd3bd36ac34954
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-01-17 02:22:11 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ class Pokemons {
 }
 const arr = [
     {
-    Img: "url('../img/pikachu.png')",
+    Img: 'img/pikachu.png',
     Name: 'Пикачу',
     Health: 100,
     Food: 100,
@@ -18,7 +18,7 @@ const arr = [
     Depression: 100,
 },
 {
-    Img: "url('../img/bulbasaur.png')",
+    Img: 'img/bulbasaur.png',
     Name: 'Бульбазаур',
     Health: 100,
     Food: 100,
@@ -26,7 +26,7 @@ const arr = [
     Depression: 100,
 },
 {
-   Img: "url('../img/dino.png')",
+   Img: 'img/dino.png',
    Name: 'Дино',
    Health: 100,
    Food: 100,
@@ -34,7 +34,7 @@ const arr = [
    Depression: 100,
 },
 {
-   Img:"url('../img/psiduck.png')",
+   Img: 'img/psiduck.png',
    Name: 'Псидак',
    Health: 100,
    Food: 100,
@@ -42,7 +42,7 @@ const arr = [
    Depression: 100,
 },
 {
-   Img: "url('../img/skqirtl.png')",
+   Img: 'img/skqirtl.png',
    Name: 'Сквиртл',
    Health: 100,
    Food: 100,
@@ -54,38 +54,49 @@ const arr = [
 function addPokemon () {
     let i = arr[(Math.floor(Math.random(0, arr.length)*(arr.length+1)))];
     console.log(i.Img)
-    let body = document.querySelector('body');
-    let divPokemon = document.createElement('div');
-    let divHealth = document.createElement('div');
-    let spanHealth = document.createElement('span');
-    let divImg = document.createElement('div');
-    let img = document.createElement('img');
-    let divName = document.createElement('div');
-    let spanName = document.createElement('span');
-    let divButtons = document.createElement('div');
-    let divFood = document.createElement('div');
-    let btnFood = document.createElement('button');
-    let divClean = document.createElement('div');
-    let btnClean = document.createElement('button');
-    let divPlay = document.createElement('div');
-    let btnPlay = document.createElement('button');
-    let divMain = document.createElement('div');
-
+    const body = document.querySelector('body');
+    const divPokemon = document.createElement('div');
+    const divHealth = document.createElement('div');
+    const spanHealth = document.createElement('span');
+    const divImg = document.createElement('div');
+    const img = document.createElement('img');
+    const divName = document.createElement('div');
+    const spanName = document.createElement('span');
+    const divButtons = document.createElement('div');
+    const divFood = document.createElement('div');
+    const divFood1 = document.createElement('div');
+    const spanFood = document.createElement('span');
+    const btnFood = document.createElement('button');
+    const divClean = document.createElement('div');
+    const divClean1 = document.createElement('div');
+    const spanClean = document.createElement('span');
+    const btnClean = document.createElement('button');
+    const divPlay = document.createElement('div');
+    const divPlay1 = document.createElement('div');
+    const spanPlay = document.createElement('span');
+    const btnPlay = document.createElement('button');
+    const divMain = document.createElement('div');
     spanName.innerText= i.Name.toUpperCase();
     img.src= i.Img;
     btnFood.innerText= 'КОРМИТЬ';
-    btnClean.innerText= 'ЧИСТИТЬ';
+    btnClean.innerText= 'УБРАТЬ';
     btnPlay.innerText= 'ИГРАТЬ';
-    spanHealth.innerText= 'ЗДОРОВЬЕ' + i.Health;
+    spanHealth.innerText= 'ЗДОРОВЬЕ' + i.Health ;
+    spanFood.innerText= 'СЫТОСТЬ'+i.Food;
+    spanClean.innerText='ГИГИЕНА'+i.Clean;
+    spanPlay.innerText='РАДОСТЬ'+i.Depression;
+    divButtons.className = 'buttons';
     divHealth.appendChild(spanHealth);
     divImg.appendChild(img);
     divName.appendChild(spanName);
+    divFood1.appendChild(spanFood);
     divFood.appendChild(btnFood);
-    divClean.appendChild(btnClean);
-    divPlay.appendChild(btnPlay); 
-    divButtons.className = 'buttons';
     divButtons.appendChild(divFood);
+    divClean1.appendChild(spanClean); 
+    divClean.appendChild(btnClean);
     divButtons.appendChild(divClean);
+    divPlay1.appendChild(spanPlay); 
+    divPlay.appendChild(btnPlay);
     divButtons.appendChild(divPlay);
     divPokemon.className = 'pokemon';
     divMain.className = 'divmain';
@@ -95,5 +106,31 @@ function addPokemon () {
     divPokemon.appendChild(divButtons);
     divMain.appendChild(divPokemon);
     body.appendChild(divMain);
+    btnFood.addEventListener('click', () => this.Eating());
+    
+    this.interval1 = setInterval(() =>{
+	
+        if (this.Food>0){
+            this.Food--;
+        }
 
-}
+        
+        if (this.Food<50 && this.Food>19) {this.Health--;}
+        else{if (this.Food<20) {this.Health=this.Health-3;}}
+
+        if (this.Food<0) {
+            this.Food=0;
+        }
+
+        this.Regener();
+        this.Dead();
+        
+    },1000);}
+
+
+    Eating(); 
+    {
+		this.Food += 10;
+		if (this.Food>100) {this.Food =100;}
+		this.Regener();
+	}
